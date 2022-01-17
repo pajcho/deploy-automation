591e39ffcda6f9303563f97a9aeb992d91b6d6f2
Nikola Pajic <nikola.pajic@gmail.com>
2022-01-17 16:53:18 +0100

    Delete old js codebase

--- functions/clear-previous-line.js
+++
@@ -1,4 +0,0 @@
-export function clearPreviousLine() {
-  process.stdout.moveCursor(0, -1); // up one line
-  process.stdout.clearLine(1); // from cursor to end
-}
--- functions/read-file.js
+++
@@ -1,7 +0,0 @@
-import chalk from 'chalk';
-import { execSync } from 'child_process';
-
-export function readFile(path) {
-  console.log(chalk.dim(`  Reading file contents: ${path}`));
-  return execSync(`touch ${path} && cat ${path}`, { stdio: 'pipe' }).toString();
-}
