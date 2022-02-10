bc52797299c4ab80303fd26070fa37439244e94f
Nikola Pajic <nikola.pajic@gmail.com>
2022-02-10 15:53:07 +0100

    Display more options when suggesting new versions

--- src/helpers/generate-suggested-versions.ts
+++ src/helpers/generate-suggested-versions.ts
@@ -7,48 +7,58 @@ import { AppVersion } from '../models/app-version.model';
  * ie. 13.2.5 -> [13.2.6, 13.2.7, 13.3.0, 14.0.0]
  */
 export function generateSuggestedVersions(currentVersion: string, isHotfix = false): AppVersion[] {
-  const nextHotfixVersion: string = currentVersion
+  if (isHotfix) {
+    return [
+      { name: `${currentVersion} (current)`, value: currentVersion },
+      { name: getHotfixVersion(currentVersion, 1) },
+      { name: getHotfixVersion(currentVersion, 2) },
+      { name: getHotfixVersion(currentVersion, 3) },
+      { name: getHotfixVersion(currentVersion, 4) },
+    ];
+  }
+
+  return [
+    { name: `${currentVersion} (current)`, value: currentVersion },
+    { name: getMinorVersion(currentVersion, 1) },
+    { name: getMinorVersion(currentVersion, 2) },
+    { name: getMinorVersion(currentVersion, 3) },
+    { name: getMajorVersion(currentVersion, 1) },
+    { name: getMajorVersion(currentVersion, 2) },
+  ];
+}
+
+function getHotfixVersion(currentVersion: string, incrementBy = 1): string {
+  return currentVersion
     .split('.')
     .map((number, index) => {
-      if (index === currentVersion.split('.').length - 1) return Number.parseInt(number, 10) + 1;
+      if (index === currentVersion.split('.').length - 1) return Number.parseInt(number, 10) + incrementBy;
 
       return number;
     })
     .join('.');
+}
 
-  const secondNextHotfixVersion: string = currentVersion
+function getMinorVersion(currentVersion: string, incrementBy = 1): string {
+  return currentVersion
     .split('.')
     .map((number, index) => {
-      if (index === currentVersion.split('.').length - 1) return Number.parseInt(number, 10) + 2;
-
-      return number;
-    })
-    .join('.');
-
-  const nextMinorVersion: string = currentVersion
-    .split('.')
-    .map((number, index) => {
-      if (index === currentVersion.split('.').length - 2) return Number.parseInt(number, 10) + 1;
+      if (index === currentVersion.split('.').length - 2) return Number.parseInt(number, 10) + incrementBy;
       if (index === currentVersion.split('.').length - 1) return 0;
 
       return number;
     })
     .join('.');
+}
 
-  const nextMajorVersion: string = currentVersion
+function getMajorVersion(currentVersion: string, incrementBy = 1): string {
+  return currentVersion
     .split('.')
     .map((number, index) => {
-      if (index === currentVersion.split('.').length - 3) return Number.parseInt(number, 10) + 1;
+      if (index === currentVersion.split('.').length - 3) return Number.parseInt(number, 10) + incrementBy;
       if (index === currentVersion.split('.').length - 2) return 0;
       if (index === currentVersion.split('.').length - 1) return 0;
 
       return number;
     })
     .join('.');
-
-  if (isHotfix) {
-    return [{ name: `${currentVersion} (current)`, value: currentVersion }, { name: nextHotfixVersion }, { name: secondNextHotfixVersion }];
-  }
-
-  return [{ name: `${currentVersion} (current)`, value: currentVersion }, { name: nextMinorVersion }, { name: nextMajorVersion }];
 }
